b4aceb7788fa8488c3991ba9385d70d5a2efb9e6
Seweryn Gładysz <seweryngla@hotmail.com>
2021-04-14 20:28:20 +0200

    create valid arduino structure

--- leclerc/leclerc.ino
+++ leclerc/leclerc.ino
@@ -1,5 +1,4 @@
-// TODO fix data library
-#include "../../leclerc.h"
+#include "leclerc.h"
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
@@ -11,19 +10,19 @@ unsigned int udpPort = 4210;
 char incomingPacket[255];
 
 void setup() {
-  WiFi.begin(ssid, password);
-  delay(500);
-  Udp.begin(udpPort);
+  // WiFi.begin(ssid, password);
+  // delay(500);
+  // Udp.begin(udpPort);
 }
 
 void loop() {
-  int packetSize = Udp.parsePacket();
+  // int packetSize = Udp.parsePacket();
 
-  if (packetSize) {
-    int len = Udp.read(incomingPacket);
+  // if (packetSize) {
+  //   int len = Udp.read(incomingPacket);
 
-    if (len > 0) {
-      incomingPacket[len] = 0;
-    }
-  }
+  //   if (len > 0) {
+  //     incomingPacket[len] = 0;
+  //   }
+  // }
 }
